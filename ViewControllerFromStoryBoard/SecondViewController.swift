d9c59b918dcdc6b84ef8cd252cacb79f27b965a9
kspland <kdspland@gmail.com>
2020-08-02 15:37:37 -0500

    Add fiies to project

---
+++ ViewControllerFromStoryBoard/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  ViewControllerFromStoryBoard
+//
+//  Created by K S on 8/2/20.
+//  Copyright © 2020 K S All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+    
+
+  
+
+}
---
+++ ViewControllerFromStoryBoard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ViewControllerFromStoryBoard
+//
+//  Created by K S on 8/2/20.
+//  Copyright © 2020 Kilbert Spland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+        
+    }
+
+    @IBAction  func didTapButton() {
+        
+        let vc = SecondViewController()
+            present(vc, animated: true)
+    }
+
+}
+
